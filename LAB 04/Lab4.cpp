3483957492542b651516c0a74e98cc36e84974a7
Slot <kluirittiphang@gmail.com>
2025-07-24 00:56:09 +0700

    sss

---
+++ LAB 04/Lab4.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <conio.h>
+
+int main() {
+char EmployeesID[10];
+int Hour  = 0;
+float salary = 0; ;
+    printf("Enter Employee ID: ");
+    scanf("%s", EmployeesID);
+    printf("the working hrs: ");
+    scanf("%d", &Hour);
+    printf("Salary amount/hr: ");
+	return 0;
+}

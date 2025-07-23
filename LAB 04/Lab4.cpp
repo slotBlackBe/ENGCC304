ee35e8e5d34aeef833f7617cdbf12f0297314f81
Slot <kluirittiphang@gmail.com>
2025-07-24 01:37:49 +0700

    Lab4

--- LAB 04/Lab4.cpp
+++ LAB 04/Lab4.cpp
@@ -1,14 +1,13 @@
 #include <stdio.h>
-#include <conio.h>
+#include <iostream>
 
 int main() {
-char EmployeesID[10];
-int Hour  = 0;
-float salary = 0; ;
-    printf("Enter Employee ID: ");
+    char EmployeesID[10];
+    int Hour = 0;
+    float salary = 0;
+    printf("the Employees ID : ");
     scanf("%s", EmployeesID);
-    printf("the working hrs: ");
+    printf("the working hrs : ");
     scanf("%d", &Hour);
-    printf("Salary amount/hr: ");
-	return 0;
+    printf("Salary amount/hr : %f\n",Hour + salary);
 }

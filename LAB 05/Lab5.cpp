5be7e9661724413f3121b826178fd2ecd0dd5217
Slot <kluirittiphang@gmail.com>
2025-07-24 14:24:43 +0700

    Lab 5

--- LAB 05/Lab5.cpp
+++ LAB 05/Lab5.cpp
@@ -6,14 +6,22 @@ int main() {
     printf("Enter your score: ");
     scanf("%d", &score);
     printf("Grade :");
-    if( score >= 80) {
+    if( score >= 90) {
         printf("A");
+    } else if (score >= 75) {
+        printf("B+");
     } else if (score >= 70) {
         printf("B");
+    } else if (score >= 65) {
+        printf("C+");
     } else if (score >= 60) {
         printf("C");
+    } else if (score >= 65) {
+        printf("D+");
     } else if (score >= 50) {
-        printf("A");
+        printf("D");    
+    } else {
+        printf("F");
     }
-    return 0; // Placeholder for the main function
+    return 0; 
 }

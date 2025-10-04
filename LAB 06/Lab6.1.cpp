c2c757c3a877c22c3aa2828e4939b02b70fab2e7
Slot <kluirittiphang@gmail.com>
2025-10-04 22:33:22 +0700

    ddd

---
+++ LAB 06/Lab6.1.cpp
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main() {
+    int num;
+
+    printf("Enter a number (0-12): ");
+    scanf("%d", &num);
+
+    switch (num)
+    {
+    case 0: printf("Zero\n");break;
+    case 1: printf("One\n");break;
+    case 2: printf("Two\n");break;
+    case 3: printf("Three\n");break;
+    case 4: printf("Four\n");break;
+    case 5: printf("Five\n");break;
+    case 6: printf("Six\n");break;
+    case 7: printf("Seven\n");break;
+    case 8: printf("Eight\n");break;
+    case 9: printf("Nine\n");break;
+    case 10: printf("Ten\n");break;
+    case 11: printf("Eleven\n");break;
+    case 12: printf("Twelve\n");break;
+    default: printf("Invalid Input (please enter 0-12\n)");
+
+
+    }
+    return 0;
+}
